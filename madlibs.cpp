7f6402540bc07465d80bef5b215edf4b9a4f1473
CarterFitzsimons <cfitzsimons1@mail.csuchico.edu>
2018-01-31 17:41:06 -0800

    Added the entire madlibs story with user inputted variables

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,23 @@
-//Authors: 
+//Author: Carter Fitzsimons 
 #include <iostream>
 #include <string>
 
 int main()
 {
+  std::string Name;
+  std::string Liquid;
+  int Number;
+  char Letter;
+  
+  std::cout<<"Enter a name."<<std::endl;
+  std::cin>>Name;
+  std::cout<<"Enter the name of a liquid."<<std::endl;
+  std::cin>>Liquid;
+  std::cout<<"Enter a number."<<std::endl;
+  std::cin>>Number;
+  std::cout<<"Enter a single letter grade."<<std::endl;
+  std::cin>>Letter;
 
+  std::cout<<"One day in class, "<<Name<<" spilled their "<<Liquid<<" on a peer's laptop.\n\"OMG\" they said, \"That cost me $"<<Number<<"!\"\nThe teacher required "<<Name<<" to replace it, or they would get an "<<Letter<<"."<<std::endl; 
   return 0;
 }
